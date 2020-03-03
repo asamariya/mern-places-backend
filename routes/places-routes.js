fbb09accbedb6f73328be43049c73c3e67f4375c
Abhishek Samariya <asamariya@gmail.com>
2020-03-03 20:23:06 +1100

    Added /:pid and /user/:uid in places-routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,41 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({ message: 'It works!' });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Bldg',
+    description: 'Another one of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => p.id === placeId);
+  res.json({ place });
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+  const places = DUMMY_PLACES.filter(p => p.creator === userId);
+  res.json({ places });
 });
 
 module.exports = router;
