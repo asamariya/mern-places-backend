bb2bccbc3cc600c64dbb25c6a0d4faedfabe459c
Abhishek Samariya <asamariya@gmail.com>
2020-03-19 07:10:35 +1100

    Installed fs for image file and updated image url in createdUser in signUp in users controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -50,7 +50,7 @@ const signup = async (req, res, next) => {
   const createdUser = new User({
     name,
     email,
-    image: 'https://avatars2.githubusercontent.com/u/25831857?s=460&v=4',
+    image: req.file.path,
     password,
     places: []
   });
