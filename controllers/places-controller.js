dcc4ca61dfd6b3368055d40470813b94d8582de7
Abhishek Samariya <asamariya@gmail.com>
2020-04-07 06:36:06 +1000

    Remove image file when deleting a place

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -3,6 +3,7 @@
 // const { uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
@@ -168,6 +169,8 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  const imagePath = place.image;
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -182,6 +185,11 @@ const deletePlace = async (req, res, next) => {
     );
     return next(error);
   }
+
+  fs.unlink(imagePath, err => {
+    console.log(err);
+  });
+
   res.status(200).json({ message: 'Deleted place.' });
 };
 
